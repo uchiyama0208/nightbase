355510acc14289117e42c48e22f0a355ec589a72
uchiyama0208 <uchiyama0208@gmail.com>
2025-11-16 02:00:25 +0900

    Fix server Supabase client typing

--- src/lib/supabaseServerClient.ts
+++ src/lib/supabaseServerClient.ts
@@ -1,11 +1,10 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import type { SupabaseClient } from "@supabase/supabase-js";
 import { cookies } from "next/headers";
 
 import type { Database } from "@/types/supabase";
 import { getSupabaseConfig } from "./supabaseClient";
 
-export function createServerClient(): SupabaseClient<Database> {
+export function createServerClient() {
   const { supabaseUrl, supabaseAnonKey } = getSupabaseConfig();
 
   return createServerComponentClient<Database>({
