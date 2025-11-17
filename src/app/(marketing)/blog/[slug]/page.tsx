ca134647844b1fd2c8f5bdef6c0e0cf5bdcc2194
uchiyama0208 <uchiyama0208@gmail.com>
2025-11-18 02:50:18 +0900

    Fix blog and case study slug routes

--- src/app/(marketing)/blog/[slug]/page.tsx
+++ src/app/(marketing)/blog/[slug]/page.tsx
@@ -1,6 +1,5 @@
 export const dynamic = "force-dynamic";
-export const dynamicParams = true;
-export const revalidate = 60;
+export const revalidate = 0;
 
 import Link from "next/link";
 import type { Metadata } from "next";
