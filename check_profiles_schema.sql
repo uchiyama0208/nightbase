dfbc82c4c569122189e58aad0de200d4da25fd66
uchiyama0208 <uchiyama0208@gmail.com>
2025-11-26 00:31:45 +0900

    chore: remove temporary SQL/debug files

--- check_profiles_schema.sql
+++
@@ -1,8 +0,0 @@
--- Check profiles table schema
-SELECT 
-    column_name,
-    data_type,
-    is_nullable
-FROM information_schema.columns
-WHERE table_name = 'profiles'
-ORDER BY ordinal_position;
